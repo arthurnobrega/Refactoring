f3c722ebb7252c18f5f762c5fb20caf27fcd26e8
Arthur Nobrega <arthurthiago@gmail.com>
2019-01-26 16:23:58 -0200

    Move functions plyFor and amountFor

--- chapter1/index.js
+++ chapter1/index.js
@@ -10,7 +10,7 @@ export default function statement(invoice, plays) {
 
     for (let perf of data.performances) {
       // print line for this order
-      result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${ perf.audience } seats)\n`;
+      result += ` ${perf.play.name}: ${usd(perf.amount)} (${ perf.audience } seats)\n`;
     }
 
     result += `Amount owed is ${usd(totalAmount())}\n`;
@@ -19,23 +19,23 @@ export default function statement(invoice, plays) {
   }
 
   function enrichPerformance(aPerformance) {
-    return {
-      ...aPerformance,
-      play: playFor(aPerformance)
-    };
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
+    result.amount = amountFor(result);
+    return result;
   }
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
-      result += amountFor(perf);
+    for (let perf of statementData.performances) {
+      result += perf.amount;
     }
     return result;
   }
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of statementData.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
@@ -51,7 +51,7 @@ export default function statement(invoice, plays) {
 
   function volumeCreditsFor(aPerformance) {
     let result = Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 
@@ -62,7 +62,7 @@ export default function statement(invoice, plays) {
   function amountFor(aPerformance) {
     let result = 0;
 
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case 'tragedy':
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -77,7 +77,7 @@ export default function statement(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unkown type: ${playFor(aPerformance).type}`);
+        throw new Error(`unkown type: ${aPerformance.play.type}`);
     }
 
     return result;
