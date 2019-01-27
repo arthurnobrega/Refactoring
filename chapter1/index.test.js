fb967c344c6168ef2441ba004333181ca7a851ab
Arthur Nobrega <arthurthiago@gmail.com>
2019-01-27 10:57:26 -0200

    Add renderHtml function

--- chapter1/index.test.js
+++ chapter1/index.test.js
@@ -1,8 +1,8 @@
 import invoices from './invoices.json';
 import plays from './plays.json';
-import statement from '.';
+import statement, { htmlStatement }from '.';
 
-test('returns correct bill', () => {
+test('returns text statement', () => {
   const bill = statement(invoices[0], plays);
 
   expect(bill).toEqualWithCompressedWhitespace(`
@@ -12,5 +12,21 @@ test('returns correct bill', () => {
      Othello: $500.00 (40 seats)
     Amount owed is $1,730.00
     You earned 47 credits
+  `);
+});
+
+test('returns html statement', () => {
+  const bill = htmlStatement(invoices[0], plays);
+
+  expect(bill).toEqualWithCompressedWhitespace(`
+    <h1>Statement for BigCo</h1>
+    <table>
+     <tr><th>play</th><th>seats</th><th>cost</th></tr>
+     <tr><td>Hamlet</td>55<td></td><td>$650.00</td></tr>
+     <tr><td>As You Like It</td>35<td></td><td>$580.00</td></tr>
+     <tr><td>Othello</td>40<td></td><td>$500.00</td></tr>
+    </table>
+    <p>Amount owed is <em>$1,730.00</em></p>
+    <p>You earned <em>47</em> credits</p>
   `);
 });
